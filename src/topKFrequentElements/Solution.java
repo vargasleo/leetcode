666d1038694c7fefd8e24c2bbbcbc27c99239178
vargasleo <imleovargas@gmail.com>
2022-04-09 16:19:17 -0300

    solving addTwoNumbers, longestSubstringWithoutRepChars

--- src/topKFrequentElements/Solution.java
+++ src/topKFrequentElements/Solution.java
@@ -6,20 +6,20 @@ import java.util.*;
  * 347. Top K Frequent Elements
  * Medium
  * Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
- *
+ * <p>
  * Example 1:
  * Input: nums = [1,1,1,2,2,3], k = 2
  * Output: [1,2]
- *
+ * <p>
  * Example 2:
  * Input: nums = [1], k = 1
  * Output: [1]
- *
+ * <p>
  * Constraints:
  * 1 <= nums.length <= 105
  * k is in the range [1, the number of unique elements in the array].
  * It is guaranteed that the answer is unique.
- *
+ * <p>
  * Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
  */
 class Solution {
--- src/twoSum/Solution.java
+++ src/twoSum/Solution.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 /**
  * 1. Two Sum
  * Easy
- *
+ * <p>
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * You can return the answer in any order.
@@ -13,21 +13,21 @@ import java.util.HashMap;
  * Input: nums = [2,7,11,15], target = 9
  * Output: [0,1]
  * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
- *
+ * <p>
  * Example 2:
  * Input: nums = [3,2,4], target = 6
  * Output: [1,2]
- *
+ * <p>
  * Example 3:
  * Input: nums = [3,3], target = 6
  * Output: [0,1]
- *
+ * <p>
  * Constraints:
- *
+ * <p>
  * 2 <= nums.length <= 104
  * -109 <= nums[i] <= 109
  * -109 <= target <= 109
- *
+ * <p>
  * Only one valid answer exists.
  * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
  */
